51c665d8bca2ab5c2b4c6d7b06c42b7bb8e5bca9
AhmedSail <sdew2sdew0592855602@gmail.com>
2025-07-07 12:01:41 +0300

    first commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,9 +1,12 @@
-import { Inter, Roboto_Mono } from "next/font/google";
-import { Lusitana } from "next/font/google";
+// لا تحتاج هذا:
+// import { lusitana } from "../../ui/fonts";
+
+import { Inter, Roboto_Mono, Lusitana } from "next/font/google";
 
 export const lusitana = Lusitana({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
+
 export const inter = Inter({ subsets: ["latin"] });
 export const roboto = Roboto_Mono({ subsets: ["latin"] });
